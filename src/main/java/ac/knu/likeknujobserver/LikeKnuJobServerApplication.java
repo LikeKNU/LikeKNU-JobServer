27490202b47865438f1df71af190342471dc5933
jcw1031 <jcw001031@gmail.com>
2023-09-12 21:17:26 +0900

    initial project

---
+++ src/main/java/ac/knu/likeknujobserver/LikeKnuJobServerApplication.java
@@ -0,0 +1,13 @@
+package ac.knu.likeknujobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeKnuJobServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeKnuJobServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/knu/likeknujobserver/LikeKnuJobServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.knu.likeknujobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeKnuJobServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
