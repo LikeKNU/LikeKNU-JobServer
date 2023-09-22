61270072a85cb39799ec99cd61d4182e4d055ce5
jcw1031 <jcw001031@gmail.com>
2023-09-22 16:40:48 +0900

    Fix: 도메인 수정

--- src/main/java/ac/knu/likeknujobserver/citybus/repository/CityBusRepository.java
+++ src/main/java/ac/knu/likeknujobserver/citybus/repository/CityBusRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface CityBusRepository extends JpaRepository<CityBus, Long> {
+public interface CityBusRepository extends JpaRepository<CityBus, String> {
 
     @EntityGraph(value = EntityGraphNames.ROUTE_BUSES)
     List<CityBus> findByIsRealtimeIsTrue();
--- src/main/java/ac/knu/likeknujobserver/citybus/repository/RouteRepository.java
+++ src/main/java/ac/knu/likeknujobserver/citybus/repository/RouteRepository.java
@@ -3,5 +3,5 @@ package ac.knu.likeknujobserver.citybus.repository;
 import ac.knu.likeknujobserver.citybus.domain.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RouteRepository extends JpaRepository<Route, Long> {
+public interface RouteRepository extends JpaRepository<Route, String> {
 }
