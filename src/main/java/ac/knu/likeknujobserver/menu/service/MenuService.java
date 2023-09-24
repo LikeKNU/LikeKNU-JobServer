4da318d4344e9c89260742722c9edd7a236cb101
ahma0 <dsd932@naver.com>
2023-09-24 22:45:23 +0900

    Fix: cache init

--- src/main/java/ac/knu/likeknujobserver/menu/service/MenuService.java
+++ src/main/java/ac/knu/likeknujobserver/menu/service/MenuService.java
@@ -4,16 +4,16 @@ import ac.knu.likeknujobserver.common.value.Campus;
 import ac.knu.likeknujobserver.menu.domain.Menu;
 import ac.knu.likeknujobserver.menu.dto.MenuMessage;
 import ac.knu.likeknujobserver.menu.repository.MenuRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +21,7 @@ public class MenuService {
 
     private final MenuRepository menuRepository;
 
-    private Map<Campus, Queue<MenuMessage>> MENU_CACHE = new ConcurrentHashMap<>();
+    private static final Map<Campus, Queue<MenuMessage>> MENU_CACHE = new ConcurrentHashMap<>();
 
     public void updateMenu(MenuMessage menuMessage) {
         if(existMenuMessage(menuMessage))
@@ -31,16 +31,17 @@ public class MenuService {
         menuRepository.save(Menu.of(menuMessage));
     }
 
+    @PostConstruct
+    void init() {
+        Stream.of(Campus.values()).forEach((Campus c) ->
+                MENU_CACHE.put(c, new ConcurrentLinkedQueue<>()));
+    }
+
     private boolean existMenuMessage(MenuMessage menuMessage) {
         return MENU_CACHE.get(menuMessage.getCampus()).contains(menuMessage);
     }
 
     private void cachingMenuMessage(MenuMessage menuMessage) {
-        if(!MENU_CACHE.containsKey(menuMessage.getCampus())) {
-            MENU_CACHE.put(menuMessage.getCampus(), new LinkedList<>(List.of(menuMessage)));
-            return;
-        }
-
         Queue<MenuMessage> menuMessages = MENU_CACHE.get(menuMessage.getCampus());
         menuMessages.add(menuMessage);
     }
@@ -48,7 +49,7 @@ public class MenuService {
     @Component
     class MenuCacheScheduler {
 
-        @Scheduled(cron = "0 0 12 ? ? THU#2 *")
+        @Scheduled(cron = "0 0 12 ? ? THU *")
         public void scheduledMenuCache() {
             for(Campus campus : Campus.values())
                 MENU_CACHE.get(campus).clear();
