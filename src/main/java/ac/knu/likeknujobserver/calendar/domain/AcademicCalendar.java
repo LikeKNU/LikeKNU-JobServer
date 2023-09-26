b82af168f1c00f4afafa8097319001f83a9139bc
ahma0 <dsd932@naver.com>
2023-09-26 20:47:34 +0900

    Feat: domain 추가, 학사일정 entity 구현

--- src/main/java/ac/knu/likeknujobserver/calendar/domain/AcademicCalendar.java
+++ src/main/java/ac/knu/likeknujobserver/calendar/domain/AcademicCalendar.java
@@ -1,5 +1,47 @@
 package ac.knu.likeknujobserver.calendar.domain;
 
+import ac.knu.likeknujobserver.calendar.dto.AcademicCalendarMessage;
+import ac.knu.likeknujobserver.common.BaseEntity;
+import ac.knu.likeknujobserver.common.value.Domain;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
 
-public class AcademicCalendar {
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "academic_calendar")
+@Getter
+public class AcademicCalendar extends BaseEntity {
+
+    @Column
+    private String content;
+
+    @Column
+    private LocalDate startDate;
+
+    @Column
+    private LocalDate endDate;
+
+    protected AcademicCalendar() {
+        super(Domain.CALENDAR);
+    }
+
+    @Builder
+    public AcademicCalendar(String content, LocalDate startDate, LocalDate endDate) {
+        this();
+        this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public static AcademicCalendar of(AcademicCalendarMessage academicCalendarMessage) {
+        return AcademicCalendar.builder()
+                .content(academicCalendarMessage.getContents())
+                .startDate(academicCalendarMessage.getStartDate())
+                .endDate(academicCalendarMessage.getEndDate())
+                .build();
+    }
 }
--- src/main/java/ac/knu/likeknujobserver/common/value/Domain.java
+++ src/main/java/ac/knu/likeknujobserver/common/value/Domain.java
@@ -2,5 +2,5 @@ package ac.knu.likeknujobserver.common.value;
 
 public enum Domain {
 
-    ANNOUNCEMENT, CITY_BUS, NOTIFICATION, DEVICE, ROUTE, MENU, CAFETERIA
+    ANNOUNCEMENT, CITY_BUS, NOTIFICATION, DEVICE, ROUTE, MENU, CAFETERIA, CALENDAR
 }
