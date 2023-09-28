8e156f136b660e4daf8b57f204549885f332f87c
ahma0 <dsd932@naver.com>
2023-09-28 19:03:38 +0900

    Feat: 캐시 생성 로직 추가

--- src/main/java/ac/knu/likeknujobserver/calendar/service/AcademicCalendarService.java
+++ src/main/java/ac/knu/likeknujobserver/calendar/service/AcademicCalendarService.java
@@ -6,9 +6,14 @@ import ac.knu.likeknujobserver.calendar.repository.AcademicCalendarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,17 @@ public class AcademicCalendarService {
 
     private final AcademicCalendarRepository academicCalendarRepository;
     private final Map<Integer, Map<Integer, Queue<AcademicCalendarMessage>>> CALENDAR_CACHE = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    void init() {
+        if(CALENDAR_CACHE.containsKey(LocalDate.now().getYear())) {
+            Map<Integer, Queue<AcademicCalendarMessage>> messageQueue = new ConcurrentHashMap<>();
+            IntStream.rangeClosed(1, 12)
+                    .forEach(i -> messageQueue.put(i, new ConcurrentLinkedQueue<>()));
+
+            CALENDAR_CACHE.put(LocalDate.now().getYear(), messageQueue);
+        }
+    }
 
     public void updateCalendar(AcademicCalendarMessage calendarMessage) {
         calendarMessage.checkYearRight();
