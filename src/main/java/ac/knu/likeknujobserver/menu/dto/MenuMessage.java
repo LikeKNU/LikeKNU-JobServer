0b4df899e561bf1056e4c7b8eaf1693ef9b30945
ahma0 <dsd932@naver.com>
2023-10-04 22:55:03 +0900

    Fix: equals 및 hashcode 재정의

--- src/main/java/ac/knu/likeknujobserver/menu/dto/MenuMessage.java
+++ src/main/java/ac/knu/likeknujobserver/menu/dto/MenuMessage.java
@@ -9,6 +9,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 public class MenuMessage {
@@ -34,6 +35,19 @@ public class MenuMessage {
         this.mealType = mealType;
         this.campus = campus;
         this.cafeteria = cafeteria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuMessage that = (MenuMessage) o;
+        return Objects.equals(menus, that.menus) && Objects.equals(date, that.date) && mealType == that.mealType && campus == that.campus && cafeteria == that.cafeteria;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menus, date, mealType, campus, cafeteria);
     }
 
     public void setMenus(String menus) {
