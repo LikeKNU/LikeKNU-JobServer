3ce7b3f2acb7cd122a97a0ab7a0be7d7e3df5cca
jcw1031 <jcw001031@gmail.com>
2023-11-20 15:28:26 +0900

    feat: AWS CodeDeploy 설정

---
+++ scripts/stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT_PATH="/home/ubuntu/job-deploy"
+JAR="$ROOT_PATH/job-application.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR)
+
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 NotFound" >> $STOP_LOG
+else
+  echo "서비스 종료 " >> $STOP_LOG
+  kill -9 "$SERVICE_PID"
+fi
