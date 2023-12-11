6d8c1fb3a79b5304c11c9e512a2192e2fc66b894
jcw1031 <jcw001031@gmail.com>
2023-12-11 11:10:22 +0900

    hotfix: 푸시알림 실패 원인 로깅

--- src/main/java/ac/knu/likeknujobserver/notification/firebase/FirebaseCloudMessage.java
+++ src/main/java/ac/knu/likeknujobserver/notification/firebase/FirebaseCloudMessage.java
@@ -3,7 +3,9 @@ package ac.knu.likeknujobserver.notification.firebase;
 import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.MessagingErrorCode;
 import com.google.firebase.messaging.MulticastMessage;
+import com.google.firebase.messaging.SendResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -26,11 +28,24 @@ public class FirebaseCloudMessage {
                 .build();
 
         try {
-            BatchResponse batchResponse = FirebaseMessaging.getInstance().sendEachForMulticast(multicastMessage);
+            BatchResponse batchResponse = FirebaseMessaging.getInstance()
+                    .sendEachForMulticast(multicastMessage);
             int successCount = batchResponse.getSuccessCount();
             log.info("Push notifications successfully sent = {}, total = {}", successCount, tokens.size());
+
+            List<MessagingErrorCode> messagingErrorCodes = getMessagingErrorCodes(batchResponse);
+            log.info("messagingErrorCodes = {}", messagingErrorCodes);
         } catch (FirebaseMessagingException e) {
             log.error("Failed to send push notifications", e);
         }
+    }
+
+    private List<MessagingErrorCode> getMessagingErrorCodes(BatchResponse batchResponse) {
+        return batchResponse.getResponses()
+                .stream()
+                .filter(response -> !response.isSuccessful())
+                .map(SendResponse::getException)
+                .map(FirebaseMessagingException::getMessagingErrorCode)
+                .toList();
     }
 }
