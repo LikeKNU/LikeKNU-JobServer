d61155f5ba7ef1544bf16dbb55d821e90e577daa
jcw1031 <jcw001031@gmail.com>
2024-05-29 21:48:51 +0900

    chore: upgrade version spring, spring boot, spring ai, and other dependencies

--- src/test/java/ac/knu/likeknujobserver/LikeKnuJobServerApplicationTests.java
+++ src/test/java/ac/knu/likeknujobserver/LikeKnuJobServerApplicationTests.java
@@ -2,7 +2,9 @@ package ac.knu.likeknujobserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LikeKnuJobServerApplicationTests {
 
